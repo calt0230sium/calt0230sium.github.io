a74fa22f6c014caac64ec19e04a8641b3044a62f
Silk <victodess@gmail.com>
2020-09-29 03:49:10 +0200

    media queries, insert files

--- scripts/insertFiles.js
+++ scripts/insertFiles.js
@@ -1,19 +1,12 @@
 let time = (new Date()).getTime();
-let update = file => file + "?n=" + time;
+let insert = file => document.write("<script src='" + file + "?n=" + time + "'></script>");
 
-function insertCss(file) {
-	let css = document.createElement("link"); 
-	css.rel = "stylesheet";
-	css.href = file;
+let scripts = [
+			   "scripts/class/Shell.js",
+			   "scripts/class/WorkList.js",
+			   "scripts/class/Work.js",
+			   "scripts/db.js",
+			   "scripts/commands.js"
+			  ];
 
-	document.head.appendChild(css);
-}
-
-function insertScript(file) {
-	document.write("<script src='" + update(file) + "'></script>");
-}
-
-let scripts = ["scripts/class/Shell.js", "scripts/class/WorkList.js", "scripts/class/Work.js", "scripts/db.js", "scripts/commands.js"];
-
-for (let i=0; i < scripts.length; i++)
-	insertScript(scripts[i]);
+for (let i=0; i < scripts.length; i++) insert(scripts[i]);
